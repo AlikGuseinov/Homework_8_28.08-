b6cf059a916ca9f3fa4896d3104892c144ba23fc
Alik <9774952689@bk.ru>
2022-08-29 00:41:38 +0300

    Задание №8

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for ( int b = 2; b <= a; b++)
+{
+    if (b % 2 == 0)
+    {
+    Console.Write ( b + " " );
+    }
+
+}
